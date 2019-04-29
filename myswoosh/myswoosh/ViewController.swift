31a3258b17adf7b3312c67162a1aef56f52718fe
Nkosikhona Bulana <bulana@Nkosikhonas-Mac-mini.local>
2019-04-29 10:28:07 +0200

    added constraints

--- myswoosh/myswoosh/ViewController.swift
+++ myswoosh/myswoosh/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var backgroundImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
